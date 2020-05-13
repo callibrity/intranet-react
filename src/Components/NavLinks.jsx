2d4f2b1955f1827fd11e38f9d36df2e3902dfeda
morellaj <alexjamesmorelli@gmail.com>
2020-05-13 14:18:55 -0400

    worked on navbar styling

--- src/Components/NavLinks.jsx
+++ src/Components/NavLinks.jsx
@@ -6,12 +6,21 @@ export default function NavLinks() {
     
     return(
         <Container>
-            <Link to="/wiki">Wiki</Link>
-            <Link to="/people">People</Link>
+            <StyledLink to="/wiki">Wiki</StyledLink>
+            <StyledLink to="/people">People</StyledLink>
         </Container>
     )
 }
 
 const Container = styled.div`
-
+    font-size: 17.6px;
+    font-weight: 600;
+    color: white;
 `
+
+const StyledLink = styled(Link)`
+    padding: 8px;
+    :hover {
+        color: #dae0e5;
+    }
+`;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,14 +7,19 @@ import {Link} from 'react-router-dom'
 
 export default function Navbar() {
     
-    const [text, setText] = useState("Hello")
+    const [text, setText] = useState()
 
     return(
         <Container>
             <Link to="/">
               <Logo src="https://www.callibrity.com/hubfs/Callibrity_December2018%20Theme/Images/callibrity-logo.png" />
             </Link>
-            <Search value={text} onChange={(e) => {setText(e.target.value)}} ></Search>
+            <SearchContainer>
+                <IconContainer>
+                    <FaSearch />
+                </IconContainer>
+                <Search placeHolder='Search' value={text} onChange={(e) => {setText(e.target.value)}} ></Search>
+            </SearchContainer>
             <NavLinks />
         </Container>
     )
@@ -22,7 +27,6 @@ export default function Navbar() {
 
 const Container = styled.div`
     background-color: #343a40;
-    color: white;
     font-size: 16px;
     padding: 8px 16px;
     display: flex;
@@ -34,10 +38,23 @@ const Logo = styled.img`
     width: 132px;
 `
 
-const Search = styled.textarea`
+const SearchContainer = styled.div`
+    display: flex;
+    align-items: center;
     background-color: white;
-    color: black;
-    width: 200px;
-    height: 20px;
+    width: 600px;
+    height: 25px;
     justify-content: left;
+    box-shadow: inset 0 1px 0 rgba(0,0,0,0.1);
+    border-color: #bbbbbb #cfcfcf #e0e0e0 #cfcfcf;
+    border-radius: 3px;
+    padding: 5px;
 `
+
+const IconContainer = styled.div``;
+
+const Search = styled.input`
+    width: 100%;
+    border: none;
+    outline: none;
+`;
