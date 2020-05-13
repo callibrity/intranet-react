113aa084deeef8aa64553a80f229164b19334c76
morellaj <alexjamesmorelli@gmail.com>
2020-05-13 13:52:02 -0400

    added global style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {Route, Switch, BrowserRouter} from 'react-router-dom'
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
 import Homepage from './components/Homepage'
 import People from './components/People'
 import Wiki from './components/Wiki'
 import Navbar from './components/Navbar'
 
+const GlobalStyle = createGlobalStyle`
+ ${normalize}
+
+ html {
+  background-color: #EEECEF;
+ }
+
+ body {
+  margin: 0;
+  font-family: 'Roboto', sans-serif;
+  min-height: 100vh;
+  position: relative;
+  font-size: 20px;
+ }
+
+ a {
+   border-radius: 5px;
+   color: inherit;
+   text-decoration: none;
+   padding: 5px;
+   cursor: pointer;
+ }
+
+ h1 {
+   font-size: inherit;
+   margin: 0;
+   font-weight: inherit;
+ }
+
+ h2 {
+   font-size: inherit;
+   font-weight: inherit;
+   margin: 0;
+ }
+
+ p {
+   margin: 0;
+ }
+
+ svg {
+  margin: 0 3px;
+ }
+
+ ul {
+   list-style-type: none;
+   padding: 0;
+   margin: 0;
+ }
+`;
+
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Navbar />
       <Switch>
         <Route exact path="/" component={Homepage} />
