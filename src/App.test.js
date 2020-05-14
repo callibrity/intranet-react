9e4ce815e7f407f47289cb1f47a5821b3adf23e8
morellaj <alexjamesmorelli@gmail.com>
2020-05-14 03:00:44 -0400

    added calendar functionality

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders announcement section', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/announcements/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders calendar section', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/calendar/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
--- src/Components/home/Calendar.jsx
+++ src/Components/home/Calendar.jsx
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components'
 import CalendarDay from './CalendarDay';
+import getCalendar from './getCalendar';
+import {FaChevronLeft, FaChevronRight} from 'react-icons/fa';
+
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
 
 export default function Calendar() {
-    const list = [];
-    for (let i = 1; i <= 35; i += 1){
-        list.push(
-            <CalendarDay day={i} />
-        )
-    }
+    const [date, setDate] = useState(new Date());
+    const calendarList = getCalendar(date.getFullYear(), date.getMonth());
+    const list = calendarList.map((ele, i) => <CalendarDay day={ele} key={i}/> );
 
     return(
         <Container>
-            {list}
+            <DateContainer>
+                <TodayButton onClick={() => setDate(new Date())}>
+                    Today
+                </TodayButton>
+                <ChangeDate>
+                    <FaChevronLeft onClick={() => setDate(new Date(date.getFullYear(), date.getMonth() - 1, 1))}/>
+                    <FaChevronRight onClick={() => setDate(new Date(date.getFullYear(), date.getMonth() + 1, 1))}/>
+                </ChangeDate>
+                <DateDisplay>
+                    {`${monthNames[date.getMonth()]} ${date.getFullYear()}`}
+                </DateDisplay>
+            </DateContainer>
+            <MonthContainer>
+                {list}
+            </MonthContainer>
         </Container>
     )
 }
 
 const Container = styled.div`
+
+`;
+
+const MonthContainer = styled.div`
     display: grid;
     grid-template-columns: repeat(7, 200px);
     grid-template-rows: repeat(5, 120px);
@@ -25,3 +46,15 @@ const Container = styled.div`
     background-color: gray;
     padding: 1px;
 `;
+
+const DateContainer = styled.div`
+    display: flex;
+    width: 300px;
+    justify-content: space-between;
+`;
+
+const TodayButton = styled.div``;
+
+const ChangeDate = styled.div``;
+
+const DateDisplay = styled.div``;
