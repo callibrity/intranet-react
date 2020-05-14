d3f7f0b894531303f67ef766f499b5f4b446e6e3
morellaj <alexjamesmorelli@gmail.com>
2020-05-14 17:00:58 -0400

    basic announcements axios request

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 import Section from './home/Section';
 import Calendar from './home/Calendar';
 
 export default function Homepage() {
+  const [text, setText] = useState('Loading');
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API_URL}/announcements`)
+      .then((res) => {
+        setText(res.data);
+      })
+      .catch((err) => {
+        setText(err);
+      });
+  }, []);
+
+  const announcements = typeof text === 'string'
+    ? text
+    : text.map((ele) => (
+      <Text>
+        {`${ele.date}: ${ele.event}`}
+      </Text>
+    ));
+
   return (
     <Container>
       <Section label="Announcements" color="#663399">
-        <div>text</div>
+        {announcements}
       </Section>
       <Section label="Calendar" color="#32CD32">
         <Calendar />
@@ -20,4 +40,8 @@ const Container = styled.div`
     display: flex;
     flex-direction: column;
     margin: 50px;
+`;
+
+const Text = styled.div`
+
 `;
