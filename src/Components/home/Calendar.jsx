39675fa18122ef66d681b71e18e73cc0aa3ccaea
morellaj <alexjamesmorelli@gmail.com>
2020-05-15 08:19:45 -0400

    added default calendar events and event testing

--- src/Components/home/Calendar.jsx
+++ src/Components/home/Calendar.jsx
@@ -8,18 +8,20 @@ import "@fullcalendar/daygrid/main.css";
 import "@fullcalendar/timegrid/main.css";
 import './calendar.css';
 
+const testEvents = [
+  { title: 'url test', start: '2020-05-10', end: '2020-05-17', url: 'https://www.callibrity.com/' },
+  { title: 'background color test', start: '2020-05-10', end: '2020-05-17', backgroundColor: 'red' },
+  { title: 'border color test', start: '2020-05-10', end: '2020-05-17', borderColor: 'red' },
+  { title: 'text color test', start: '2020-05-10', end: '2020-05-17', textColor: 'red' }
+];
+
 
 export default function Calendar() {
   return (
     <FullCalendar
       defaultView="dayGridMonth"
       plugins={[dayGridPlugin, timeGridPlugin]}
-      events={[
-        { title: 'event 1', start: '2020-05-01', end: '2020-05-04' },
-        { title: 'event 2', start: '2020-05-03', end: '2020-05-06' },
-        { title: 'event 3', start: '2020-05-03', end: '2020-05-06' },
-        { title: 'event 4', start: '2020-05-03', end: '2020-05-06' },
-      ]}
+      eventSources={[`${process.env.REACT_APP_API_URL}/calendar`, testEvents]}
       header={{
         left: "prev,next today",
         center: "title",
@@ -28,3 +30,12 @@ export default function Calendar() {
     />
   );
 }
+
+/*
+[
+        { title: 'event 1', start: '2020-05-01', end: '2020-05-04' },
+        { title: 'event 2', start: '2020-05-03', end: '2020-05-06' },
+        { title: 'event 3', start: '2020-05-03', end: '2020-05-06' },
+        { title: 'event 4', start: '2020-05-03', end: '2020-05-06' },
+      ]
+      */
