ee97ad19aae362fcf2275c2a43e81e98e54a6df0
Alex Meyer <ameyer@callibrity.com>
2020-05-18 15:42:11 -0400

    Use key prop in component creation for-loop

--- src/Components/people/People.jsx
+++ src/Components/people/People.jsx
@@ -8,7 +8,7 @@ function renderPeople() {
   // eslint-disable-next-line array-callback-return
   tempData.map((data) => {
     output.push(
-      <PersonTile name={data.name} role={data.role} photo={data.photo} />
+      <PersonTile key={data.photo} name={data.name} role={data.role} photo={data.photo} />
     );
   });
   return output;
