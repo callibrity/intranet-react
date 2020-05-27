c1624c0e4f9961c3e09d4355d3a295ae332baa45
morellaj <alexjamesmorelli@gmail.com>
2020-05-27 12:35:36 -0400

    small change

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import NavLinks from "./NavLinks";
 import { Link } from "react-router-dom";
 import { UserContext } from "./UserContext";
 import Logout from "./Logout";
-import Dropdown from "react-bootstrap";
+import {Dropdown} from "react-bootstrap";
 
 
 
