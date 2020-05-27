40bbbe38f4a6f72d269aef1f91968ce6f538fc48
morellaj <alexjamesmorelli@gmail.com>
2020-05-27 18:02:55 -0400

    improved embedded google calendar

--- src/Components/home/Calendar.jsx
+++ src/Components/home/Calendar.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import styled from "styled-components";
 
 export default function Calendar() {
+  const baseStr = "https://calendar.google.com/calendar/b/2/embed?showTz=0&showCalendars=1&showPrint=0&showTitle=0";
+  const companyHolidays = "Y2FsbGlicml0eS5jb21fMGY5aXJjMDFrOGowZ3A5ZG9xbTRvZGpvcm9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ";
+  const lunchAndLearn = "Y2FsbGlicml0eS5jb21fYnM0b3U4NDB0dnVva2w2N2JhMG50MjZycGtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ";
+  const arr = [companyHolidays, lunchAndLearn];
+  const str = arr.reduce((acc, value) => `${acc}&src=${value}`, baseStr);
+
   return (
     <Container>
       <iframe 
         title="calendar" 
-        src="https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;showTitle=0&amp;showPrint=0&amp;src=Y2FsbGlicml0eS5jb21fcHE1dDVlYWNyczVoN29haHNhOWk3bTJtNm9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=Y2FsbGlicml0eS5jb21fMGY5aXJjMDFrOGowZ3A5ZG9xbTRvZGpvcm9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=Y2FsbGlicml0eS5jb21fYnM0b3U4NDB0dnVva2w2N2JhMG50MjZycGtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23F6BF26&amp;color=%23009688&amp;color=%23F6BF26" 
+        src={str}
         width="100%" 
         height="100%" 
         frameBorder="0" 
-        scrolling="no" />
+        scrolling="no" 
+      />
     </Container>
   );
 }
