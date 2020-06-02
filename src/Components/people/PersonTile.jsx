b5b3de0b065264b3b11f1d31705df00ec45aff27
morellaj <alexjamesmorelli@gmail.com>
2020-06-02 11:50:55 -0400

    linked people to profile pages

--- src/Components/people/PersonTile.jsx
+++ src/Components/people/PersonTile.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./PersonTile.css";
 
 export default function PersonTile({ name, role, photo }) {
   return (
-    <div className="card tile">
-      <img className="card-img-top" src={photo} alt={name} />
-      <div className="card-text tile-overlay">
-        <span className="tile-overlay-name">{name}</span>
-        <br />
-        <span>{role}</span>
+    <Link to={`/personal?name=${name}`}>
+      <div className="card tile">
+        <img className="card-img-top" src={photo} alt={name} />
+        <div className="card-text tile-overlay">
+          <span className="tile-overlay-name">{name}</span>
+          <br />
+          <span>{role}</span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
